772fee35f740fc4264dfe5855d8386b4f92a2fc3
dementriadelvin <148644890+StArViNn@users.noreply.github.com>
2024-01-04 15:24:30 +0700

    sec script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 data =[{
     'img1':'asset/Desain tanpa judul (1).png',
     'img2':'asset/Desain tanpa judul 10 .png',
-    'title':'Colase',
-    'subtitle':'Zenit',
-    'desc': "dawdwadadwadwadwa."
-},{    
-    'img1':'asset/Desain tanpa judul 5 .png',
-    'img2':'asset/Desain tanpa judul 11 .png',
-    'title':'Colase',
-    'subtitle':'Zenit',
-    'desc': "dawdwadadwadwadwa."
-    
+    'title':'Kolase Oshi',
+    'subtitle':'Wallpaper Desktop',
+    'desc': ""
 }]
+data2 =[{
+    'img1':'asset/desain 720x1280 (3) (1).png',
+    'img2':'asset/desain 720x1280 (4) (1).png',
+    'title':'Kolase Oshi',
+    'subtitle':'Wallpaper Handphone',
+    'desc': ""
+}]
+    
+
+
 var container =document.querySelector('.container-project')
 
 data.forEach(element => {
@@ -28,5 +31,19 @@ data.forEach(element => {
             </div>
             <button class="read-more">Read More</button>
         </div>`
-    
+});
+data2.forEach(element => {
+    container.innerHTML +=
+    `<div class="project">
+            <div class="img-project-2">
+                        <img src="`+element.img1+`" alt="">
+                        <img src="${element.img2}" alt="">
+                    </div>
+                    <div class="desc-project-2">
+                        <h1>${element.title}</h1>
+                        <h2>${element.subtitle}</h2>
+                        <p>${element.desc}</p>
+                    </div>
+                    <button class="read-more">Read More</button>
+                </div>`            
 });
